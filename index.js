3bff00d721f72d53ed8289657933b4e3b633ad1e
kevincollao <kevin.collao1@gmail.com>
2022-02-05 20:47:00 -0300

    ..

--- index.js
+++ index.js
@@ -29,13 +29,52 @@ const miContador = (function(){
         valor
     }
 })();
-
-console.log(miContador.incrementar())
-console.log(miContador.incrementar())
+//console.log(typeof(miContador))
+//console.log(miContador.valor())
 //console.log(miContador.incrementar())
-//
-console.log(miContador.valor())
-console.log(miContador.valor())
-console.log(miContador.valor())
-console.log(miContador.valor())
-console.log(miContador.valor())
+//console.log(miContador.incrementar())
+//console.log(miContador.incrementar())
+//console.log(miContador.incrementar())
+
+function externa(){
+    var saludar = "Hola como estás"
+    return function interna(){
+        return saludar
+    }
+}
+//el doble paréntesis ejecuta la función externa e interna
+//La función interna tiene acceso a las variables definidas en la función externa. Y solo está disponible si se llama a la función padre
+console.log(externa()())
+
+
+//CLOSURE DE FUNCIÓN SUMA
+function suma(a){
+    return function calculo(b){
+        return a+b;
+    }
+}
+console.log(suma(2)(6))
+// 4 FUNCIONES ANIDADAS
+const x = "ABCD";
+const fe = () => {
+    const y = "EFGH";
+    //
+    const fi = () => {
+        const z = "IJK";
+        //
+        const fii = () => {
+            const u = "LMN"
+            //
+            const fiii = () =>{
+                const p = "OPQ";
+                return x + y + z + u + p;
+            }
+            return fiii;
+        }
+        //
+        return fii;
+    }
+    //
+    return fi;
+}
+console.log(fe()()()())
